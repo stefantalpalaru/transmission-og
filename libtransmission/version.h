cb1b50528972e7e2e055df971d40ee4e3149bba0
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-04-29 15:03:03 +0200

    -framework Cocoa

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "db38fd85cc"
-#define VCS_REVISION_NUM          db38fd85cc
+#define VCS_REVISION              "aa63532a46"
+#define VCS_REVISION_NUM          aa63532a46
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (db38fd85cc)"
+#define LONG_VERSION_STRING       "3.01+ (aa63532a46)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
