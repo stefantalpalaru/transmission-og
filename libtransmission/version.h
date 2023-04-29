db38fd85cca46f7f78e4e1d277088040f7738a82
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-04-29 04:07:18 +0200

    brew update

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "9e5c26c298"
-#define VCS_REVISION_NUM          9e5c26c298
+#define VCS_REVISION              "c54048c94f"
+#define VCS_REVISION_NUM          c54048c94f
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (9e5c26c298)"
+#define LONG_VERSION_STRING       "3.01+ (c54048c94f)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
