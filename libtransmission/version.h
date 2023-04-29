e1533f80cbee0f56a4cae6093d0becef15237b61
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-04-29 19:27:05 +0200

    fix HAVE_FLOCK

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "55872439f1"
-#define VCS_REVISION_NUM          55872439f1
+#define VCS_REVISION              "3b38391093"
+#define VCS_REVISION_NUM          3b38391093
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (55872439f1)"
+#define LONG_VERSION_STRING       "3.01+ (3b38391093)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
