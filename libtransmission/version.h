55872439f1298268a772a531fd0001676a0a6c01
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-04-29 18:02:59 +0200

    Qt: proper INCLUDEPATH

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "aa63532a46"
-#define VCS_REVISION_NUM          aa63532a46
+#define VCS_REVISION              "cb1b505289"
+#define VCS_REVISION_NUM          cb1b505289
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (aa63532a46)"
+#define LONG_VERSION_STRING       "3.01+ (cb1b505289)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
