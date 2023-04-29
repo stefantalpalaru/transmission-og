cb888b35407f9d4a1ab5c6205193fe10d1554d28
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-04-29 18:22:49 +0200

    macOS fix

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "cb1b505289"
-#define VCS_REVISION_NUM          cb1b505289
+#define VCS_REVISION              "55872439f1"
+#define VCS_REVISION_NUM          55872439f1
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (cb1b505289)"
+#define LONG_VERSION_STRING       "3.01+ (55872439f1)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
