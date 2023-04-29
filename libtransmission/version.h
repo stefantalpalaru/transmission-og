aa63532a46ec3972ef13ee07ff3bea940a975aa1
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-04-29 04:54:29 +0200

    32-bit Qt fix

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "c54048c94f"
-#define VCS_REVISION_NUM          c54048c94f
+#define VCS_REVISION              "db38fd85cc"
+#define VCS_REVISION_NUM          db38fd85cc
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (c54048c94f)"
+#define LONG_VERSION_STRING       "3.01+ (db38fd85cc)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
