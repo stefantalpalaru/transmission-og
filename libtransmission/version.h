1d153e7954ad55ce4e8b68e9637027256755f9a5
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-05-01 18:40:44 +0200

    define MINIUPNP_STATICLIB and NATPMP_STATICLIB

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "ca7defbd9b"
-#define VCS_REVISION_NUM          ca7defbd9b
+#define VCS_REVISION              "fbd99322a9"
+#define VCS_REVISION_NUM          fbd99322a9
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (ca7defbd9b)"
+#define LONG_VERSION_STRING       "3.01+ (fbd99322a9)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
