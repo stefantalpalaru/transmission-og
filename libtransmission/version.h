43521ad5486cd7054ad77214d2341f53c1c387a4
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-05-01 14:57:40 +0200

    don't override PATH

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "e1533f80cb"
-#define VCS_REVISION_NUM          e1533f80cb
+#define VCS_REVISION              "e87b71c243"
+#define VCS_REVISION_NUM          e87b71c243
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (e1533f80cb)"
+#define LONG_VERSION_STRING       "3.01+ (e87b71c243)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
