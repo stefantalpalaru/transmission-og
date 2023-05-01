f99758f1c4b8173ab99696ada82a1f891de972cb
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-05-01 22:08:38 +0200

    fix subprocess-test

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "1d153e7954"
-#define VCS_REVISION_NUM          1d153e7954
+#define VCS_REVISION              "64bba77d55"
+#define VCS_REVISION_NUM          64bba77d55
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (1d153e7954)"
+#define LONG_VERSION_STRING       "3.01+ (64bba77d55)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
