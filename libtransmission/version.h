ca7defbd9bf3a7a2a72917ce899f39efb0708cba
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-05-01 15:27:39 +0200

    Windows: -luuid -lole32

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "e87b71c243"
-#define VCS_REVISION_NUM          e87b71c243
+#define VCS_REVISION              "69f2d7244c"
+#define VCS_REVISION_NUM          69f2d7244c
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (e87b71c243)"
+#define LONG_VERSION_STRING       "3.01+ (69f2d7244c)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
