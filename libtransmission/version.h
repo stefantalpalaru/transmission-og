36e20e0f7debee13a5f6040fb39f17419179f7eb
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-05-08 01:17:11 +0200

    Windows fix

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "0a6795e2be"
-#define VCS_REVISION_NUM          0a6795e2be
+#define VCS_REVISION              "ef6d40bc52"
+#define VCS_REVISION_NUM          ef6d40bc52
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (0a6795e2be)"
+#define LONG_VERSION_STRING       "3.01+ (ef6d40bc52)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
