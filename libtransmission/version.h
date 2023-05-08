dfb5f73eaaf6b867fef63b7c64dfda97f33ebb8c
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2023-05-08 05:50:41 +0200

    fix client ID expansion

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -2,10 +2,10 @@
 
 #define PEERID_PREFIX             "-TR301Z-"
 #define USERAGENT_PREFIX          "3.01+"
-#define VCS_REVISION              "ef6d40bc52"
-#define VCS_REVISION_NUM          ef6d40bc52
+#define VCS_REVISION              "522ed0417d"
+#define VCS_REVISION_NUM          522ed0417d
 #define SHORT_VERSION_STRING      "3.01+"
-#define LONG_VERSION_STRING       "3.01+ (ef6d40bc52)"
+#define LONG_VERSION_STRING       "3.01+ (522ed0417d)"
 #define VERSION_STRING_INFOPLIST  3.01+
 #define BUILD_STRING_INFOPLIST    14714.3.1
 #define MAJOR_VERSION             3
