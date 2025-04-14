2b1ec4652b532efbc9b9115c5a83befca38fa4ea
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2025-04-14 21:39:29 +0200

    fix winres syntax

--- update-version-h.sh
+++ update-version-h.sh
@@ -54,7 +54,9 @@ cat > libtransmission/version.h.new << EOF
 #define VERSION_STRING_INFOPLIST  ${user_agent_prefix}
 #define BUILD_STRING_INFOPLIST    14714.${major_version}.${minor_version}
 #define MAJOR_VERSION             "${major_version}"
+#define MAJOR_VERSION_NUM         ${major_version}
 #define MINOR_VERSION             "${minor_version}"
+#define MINOR_VERSION_NUM         ${minor_version}
 EOF
 
 # Add a release definition
